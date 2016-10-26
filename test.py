18519b7dea02f6ad905e1919591b91a994377b5f
anthonyanthonyanthony <alqokightnv@icloud.com>
2016-10-26 10:05:25 +0000

    docstring added and modified again

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
-print('bread and eggs yaaaaaay!')
+print('Fuck You World!')
 
 import numpy as np
 
 def circumference(r):
     circ = 2. * np.pi * r
     print('The circumference is', circ)
-    """random"""
     
 def area(r):
-    """This is apparently a docstring"""
+    """This is apparently a docstring. We are confused"""
     surface = np.pi * r**2
     print('The surface is', surface)
     
